f3d297ebfda27fc9a3f65de65f798ee4c50a1e4b
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-09-27 20:50:51 +0530

    add fmt tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,16 +1,16 @@
 package auth
 
 import (
-    "testing"
-    "net/http"
+	"net/http"
+	"testing"
 )
 
 func TestGetApiKey(t *testing.T) {
-    header := http.Header{}
-    header.Add("Authorization", "ApiKey someApiKeyHereYayyyy")
-    have, _ := GetAPIKey(header)
-    want := "someApiKeyHereYayyyy"
-    if have != want { 
-        t.Fatalf("expected %v, got %v", want, have)
-    }
+	header := http.Header{}
+	header.Add("Authorization", "ApiKey someApiKeyHereYayyyy")
+	have, _ := GetAPIKey(header)
+	want := "someApiKeyHereYayyyy"
+	if have != want {
+		t.Fatalf("expected %v, got %v", want, have)
+	}
 }
